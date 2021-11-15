df640805ecd3902e09231534b8b7ca2d6a0c2c08
buiphuong <buithiphuong07031999@gmail.com>
2021-11-15 22:34:28 +0700

    talent

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -85,7 +85,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <a href="{{ url('/talent') }}" class="nav-link">
                             <i class="nav-icon fas fa-th"></i>
                             <p>
                                 Talent
